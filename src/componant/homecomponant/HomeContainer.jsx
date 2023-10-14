b5947911c046aea502306bf3050f14ed892bca5d
priyankara senarathne <priyankara.sliit@gmail.com>
2023-10-14 21:41:23 +0200

    color dznami

--- src/componant/homecomponant/HomeContainer.jsx
+++ src/componant/homecomponant/HomeContainer.jsx
@@ -20,33 +20,26 @@ export default HomeContainer;
 const ColorFilter = () => {
   const [filter, setFilter] = useFilter();
   const [filterColor, setFilterColor] = useColor();
-  console.log("filterColor", filterColor);
-  const ColorChange = () => {
-    setFilter((pre) => {
-      const temp = { ...pre };
 
-      return temp;
-    });
-  };
+  filterColor.map((val, index) => {
+    console.log(val[index].Color);
+  });
 
   return (
     <div className="w-full p-2">
       <h1 className="text-left text-lg font-semibold">Color</h1>
       <div className="w-full grid grid-cols-1 lg:grid-cols-[50%_50%] mt-2">
         {/* left color */}
-        <div className="flex w-full gap-1 mt-1" onClick={() => ColorChange}>
-          <div className="flex w-[20px]  h-[20px] border-2 border-indigo-500 rounded-full items-center justify-center">
-            <div className="w-[15px] h-[15px] border-2 border-white rounded-full bg-red-700" />
-          </div>
-          <p className="items-center">Red</p>
-        </div>
-        {/* right color */}
-        <div className="flex w-full gap-1 mt-1">
-          <div className="flex w-[20px]  h-[20px] border-2 border-indigo-500 rounded-full items-center justify-center">
-            <div className="w-[15px] h-[15px] border-2 border-white rounded-full bg-red-700" />
-          </div>
-          <p className="items-center">Red</p>
-        </div>
+        {filterColor.map((iColor, index) => {
+          return (
+            <div key={index} className="flex w-full gap-1 mt-1">
+              <div className="flex w-fit  h-fit p-1 border-2 border-indigo-500 rounded-full items-center justify-center">
+                <div className="w-[20px] h-[20px] border-2 border-white rounded-full bg-red-700" />
+              </div>
+              <p>{iColor[0].Color}</p>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
--- src/context/ColorContext.jsx
+++ src/context/ColorContext.jsx
@@ -3,9 +3,14 @@ import { createContext, useContext, useState } from "react";
 export const ColorContext = createContext({});
 
 const ColorProvider = ({ children }) => {
-  const [filterColor, setFilterColor] = useState({
-    color: "red",
-  });
+  const [filterColor, setFilterColor] = useState([
+    [
+      { Color: "Red", age: 25 },
+      { Color: "Green", age: 30 },
+      { Color: "Blue", age: 35 },
+      { Color: "White", age: 35 },
+    ],
+  ]);
   return (
     <ColorContext.Provider value={{ filterColor, setFilterColor }}>
       {children}
